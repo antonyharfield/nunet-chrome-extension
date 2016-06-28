07ec3a7856ab0b2a8aa0e84ba5f717230f81d592
Tanakorn Amnoeywiboolpol <l3irdWings@gmail.com>
2016-06-28 14:54:00 +0700

    update the version 0.0.4 to connect new server authen3.nu.ac.th

--- background.js
+++ background.js
@@ -1,63 +1,107 @@
+var __VIEWSTATE = "";
+var __VIEWSTATEGENERATOR = "";
+var __EVENTVALIDATION = "";
+
 function checkForInternet() {
 	console.log("checking for internet connection...");
 	
-	pingGoogle(function() {
-		console.log("we are online");
+	pingMCL(function() {
+        console.log("we are online");
 		chrome.browserAction.setIcon({path: "icon.png"});
 		
 	}, function(respText) {
-		console.log("no internet!")
+		console.log("we are offline!");
 		chrome.browserAction.setIcon({path: "icon-offline.png"});
-							
-		// Try find a token in the NU-NET auth page
-		var token = grabTokenNuNet(respText);
-				
-		// If found then lets attempt an auth
-		if (token) {
-			console.log("NU-NET detected: " + token);
-			connectNuNet(token);
-		}
+        getValueFromNuNet();
 	});
 }
 
-function pingGoogle(success, fail) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "http://clients3.google.com/generate_204", true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if (xhr.status === 204) {
+function pingMCL(success, fail) {
+	var xhrCheck = new XMLHttpRequest();
+	xhrCheck.open("GET", "http://www.mobcomlab.com/?_=" + new Date().getTime(), true);
+	xhrCheck.onreadystatechange = function() {
+        console.log(xhrCheck);
+        console.log("Pass1"+xhrCheck.readyState+" "+xhrCheck.status);
+		if (xhrCheck.readyState == 4) {
+			if (xhrCheck.status === 200 && xhrCheck.responseURL.includes("nu.ac.th") == false) {
+                console.log("Pass2");
 				success();
 			}
-			else {
-				fail(xhr.responseText);
+            else {
+                console.log("Pass3");
+				fail(xhrCheck.responseText);
 			}
 		}
 	};
-	xhr.send();
+	xhrCheck.send();
 }
 
-function grabTokenNuNet(respText) {
-	var pattern = new RegExp('magic" value="[a-z0-9]+');
-	var token = pattern.exec(respText);
-	if (token === null)
-		return null;
-	
-	token = token[0].substr(14);
-	return token;
+function getValueFromNuNet() {
+    var xhrGet = new XMLHttpRequest();
+    xhrGet.open("GET", "https://authen3.nu.ac.th/Login.aspx", true);
+    xhrGet.onreadystatechange = function() {
+        if (xhrGet.readyState == 4) {
+            if (xhrGet.status === 200) {
+                var el = document.createElement('html');
+                el.innerHTML = xhrGet.responseText;
+                var input = el.getElementsByTagName("INPUT");
+                getViewState(input);
+                connectNuNet();
+            }
+        }
+    };
+    xhrGet.send();
 }
 
-function connectNuNet(token) {
+function getViewState(input) {
+    for (var i = 0; i < input.length; i++) {
+        switch (input[i].name) {
+            case "__VIEWSTATE":
+                __VIEWSTATE = replaceOperand(input[i].value);
+                break;
+            case "__VIEWSTATEGENERATOR":
+                __VIEWSTATEGENERATOR = replaceOperand(input[i].value);
+                break;
+            case "__EVENTVALIDATION":
+                __EVENTVALIDATION = replaceOperand(input[i].value);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+function replaceOperand(str) {
+    for (var i = 0; i < str.length; i++) {
+        switch (str.charAt(i)) {
+            case "/":
+                str = str.replace("/", "%2F");
+                break;
+            case "+":
+                str = str.replace("+", "%2B");
+                break;
+            case "=":
+                str = str.replace("=", "%3D");
+                break;
+            default:
+                break;
+        }
+    }
+    return str;
+}
+
+function connectNuNet() {
 	console.log("connecting to NU-NET");
 
     var username = localStorage["username"];
     var password = localStorage["password"];
-	
+
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "https://login.nu.ac.th:1000/", true);
+	xhr.open("POST", "https://authen3.nu.ac.th/login.aspx?v=lo", true);
 	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
-			pingGoogle(function() {
+			pingMCL(function() {
 				console.log("connect success");
 				chrome.browserAction.setIcon({path: "icon.png"});
 			}, function(respText) {
@@ -65,8 +109,8 @@ function connectNuNet(token) {
 			});
 		}
 	};
-	xhr.send("magic="+token+"&username="+username+"&password="+password);
-}
+    xhr.send("__VIEWSTATE="+__VIEWSTATE+"&__VIEWSTATEGENERATOR="+__VIEWSTATEGENERATOR+"&__EVENTVALIDATION="+__EVENTVALIDATION+"&txtUsername="+username+"&txtPassword="+password+"&btnLogin=Login");
+   }
 	
 function connectNuWireless() {
 	
@@ -88,5 +132,5 @@ chrome.windows.onCreated.addListener(checkForInternet);
 chrome.alarms.onAlarm.addListener(function() {
   checkForInternet();
 });
-chrome.alarms.create({when: 0, periodInMinutes: 3});
+chrome.alarms.create({when: 0, periodInMinutes: 1});
 
