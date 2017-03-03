a322ece169b639e802fb0328d0f3cc0e48bd338f
Thanakorn Amnuaywiboolpol <l3irdWings@gmail.com>
2017-03-03 11:18:47 +0700

    Update version 0.0.5

--- background.js
+++ background.js
@@ -1,114 +1,84 @@
-var __VIEWSTATE = "";
-var __VIEWSTATEGENERATOR = "";
-var __EVENTVALIDATION = "";
-
 function checkForInternet() {
 	console.log("checking for internet connection...");
-	
-	pingMCL(function() {
+
+    pingMCL(function() {
         console.log("we are online");
-		chrome.browserAction.setIcon({path: "icon.png"});
-		
-	}, function(respText) {
-		console.log("we are offline!");
-		chrome.browserAction.setIcon({path: "icon-offline.png"});
-        getValueFromNuNet();
-	});
+        chrome.browserAction.setIcon({path: "icon.png"});
+    }, function(respText) {
+        console.log("no internet!");
+        chrome.browserAction.setIcon({path: "icon-offline.png"});
+
+        // Try find a token in the NU-NET auth page
+        var token = grabTokenNuNet(respText);
+
+        // If found then lets attempt an auth
+        if (token) {
+            console.log("NU-NET detected: " + token);
+            connectNuNet(token);
+        }
+    });
 }
 
 function pingMCL(success, fail) {
-	var xhrCheck = new XMLHttpRequest();
-	xhrCheck.open("GET", "http://mobcomlab.s3.amazonaws.com/pingtest.json?_=" + new Date().getTime(), true);
-	xhrCheck.onreadystatechange = function() {
-		if (xhrCheck.readyState == 4 && xhrCheck.status === 200) {
+    var xhrCheck = new XMLHttpRequest();
+    var url = "http://mobcomlab.s3.amazonaws.com/pingtest.json?_=" + new Date().getTime();
+    console.log(url);
+    xhrCheck.open("GET", "http://mobcomlab.s3.amazonaws.com/pingtest.json?_=" + new Date().getTime(), true);
+    xhrCheck.onreadystatechange = function() {
+        if (xhrCheck.readyState == 4 && xhrCheck.status === 200) {
             try {
                 if (JSON.parse(xhrCheck.responseText).success) {
-                    success()
+                    success();
                 }
             } catch (e) {
                 fail(xhrCheck.responseText);
             }
-		}
-	};
-	xhrCheck.send();
-}
-
-function getValueFromNuNet() {
-    var xhrGet = new XMLHttpRequest();
-    xhrGet.open("GET", "https://authen3.nu.ac.th/Login.aspx", true);
-    xhrGet.onreadystatechange = function() {
-        if (xhrGet.readyState == 4 && xhrGet.status === 200) {
-            var el = document.createElement('html');
-            el.innerHTML = xhrGet.responseText;
-            var input = el.getElementsByTagName("INPUT");
-            getViewState(input);
-            connectNuNet();
+        }
+        else {
+            fail("");
         }
     };
-    xhrGet.send();
+    xhrCheck.send();
 }
 
-function getViewState(input) {
-    for (var i = 0; i < input.length; i++) {
-        switch (input[i].name) {
-            case "__VIEWSTATE":
-                __VIEWSTATE = replaceOperand(input[i].value);
-                break;
-            case "__VIEWSTATEGENERATOR":
-                __VIEWSTATEGENERATOR = replaceOperand(input[i].value);
-                break;
-            case "__EVENTVALIDATION":
-                __EVENTVALIDATION = replaceOperand(input[i].value);
-                break;
-            default:
-                break;
-        }
+
+function grabTokenNuNet(respText) {
+    console.log("Getting token from NU-NET");
+    var pattern = new RegExp('magic" value="[a-z0-9]+');
+    var token = pattern.exec(respText);
+    if (token === null) {
+        console.log("Token from NU-NET: null");
+        return null;
     }
+
+    token = token[0].substr(14);
+    return token;
 }
 
-function replaceOperand(str) {
-    for (var i = 0; i < str.length; i++) {
-        switch (str.charAt(i)) {
-            case "/":
-                str = str.replace("/", "%2F");
-                break;
-            case "+":
-                str = str.replace("+", "%2B");
-                break;
-            case "=":
-                str = str.replace("=", "%3D");
-                break;
-            default:
-                break;
-        }
-    }
-    return str;
-}
-
-function connectNuNet() {
-	console.log("connecting to NU-NET");
+function connectNuNet(token) {
+    console.log("connecting to NU-NET");
 
     var username = localStorage["username"];
     var password = localStorage["password"];
 
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "https://authen3.nu.ac.th/login.aspx?v=lo", true);
-	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			pingMCL(function() {
-				console.log("connect success");
-				chrome.browserAction.setIcon({path: "icon.png"});
-			}, function(respText) {
-				console.log("connect unsuccessful, maybe not NU");
-			});
-		}
-	};
-    xhr.send("__VIEWSTATE="+__VIEWSTATE+"&__VIEWSTATEGENERATOR="+__VIEWSTATEGENERATOR+"&__EVENTVALIDATION="+__EVENTVALIDATION+"&txtUsername="+username+"&txtPassword="+password+"&btnLogin=Login");
-   }
-	
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://login3.nu.ac.th:1000/", true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            pingMCL(function() {
+                console.log("connect success");
+                chrome.browserAction.setIcon({path: "icon.png"});
+            }, function(respText) {
+                console.log("connect unsuccessful, maybe not NU");
+            });
+        }
+    };
+    xhr.send("magic="+token+"&username="+username+"&password="+password);
+}
+
 function connectNuWireless() {
-	
+
 }
 
 // Default icon is offline
@@ -120,8 +90,12 @@ chrome.browserAction.onClicked.addListener(function() {
 });
 
 // Check for internet when the first tab is loaded
-chrome.tabs.onCreated.addListener(checkForInternet);
-chrome.windows.onCreated.addListener(checkForInternet);
+chrome.tabs.onCreated.addListener(function() {
+    checkForInternet();
+});
+chrome.windows.onCreated.addListener(function() {
+    checkForInternet();
+});
 
 // Set up alarm to check internet every 3 minutes
 chrome.alarms.onAlarm.addListener(function() {
